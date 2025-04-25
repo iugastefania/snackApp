6b5e34c74e2e1f0e9fdc047e7debb16bd2dac355
iugastefania <sallystefy@gmail.com>
2025-04-25 16:33:40 +0300

    accept ingredient without unit

--- api/models/ingredient.py
+++ api/models/ingredient.py
@@ -5,7 +5,7 @@ class Ingredient(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    unit = db.Column(db.String(20), nullable=False)
+    unit = db.Column(db.String(20), nullable=True)
     quantity = db.Column(db.Float, nullable=False)
 
     recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)

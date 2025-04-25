e56cab06ccde483692c16176f73f51f841bdf4ca
iugastefania <sallystefy@gmail.com>
2025-04-25 16:22:09 +0300

    fix load dotenv before using env

--- api/app.py
+++ api/app.py
@@ -1,13 +1,14 @@
 from flask import Flask
 from dotenv import load_dotenv
+
+load_dotenv()
+
 from config import Config
 from models import db
 from models.category import Category
 from models.recipe import Recipe
 from models.ingredient import Ingredient
 from models.association import recipe_category
-
-load_dotenv()
 
 app = Flask(__name__)
 app.config.from_object(Config)
